58af893ad3ec9b709b883c1540132086ac34fc64
svalvastic <assogames2@gmail.com>
2022-12-05 11:15:57 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::middleware('auth')->
+namespace('Admin')->
+name('admin.')->
+prefix('admin')->
+group(function () {
+    Route::get('/', function() {
+        return view('admin.index');
+    })->name('index');
+});
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '.*');
