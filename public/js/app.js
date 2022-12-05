8e9ec6227937c6690fb62c9c6ec61746ae21fa96
svalvastic <assogames2@gmail.com>
2022-12-05 14:04:15 +0100

    🎉 init(faker): add faker

--- public/js/app.js
+++ public/js/app.js
@@ -49729,8 +49729,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Valerio\Desktop\Boolean\Lezioni-back-end-PHP-MAMP\Progetto Finale\Boover-project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Valerio\Desktop\Boolean\Lezioni-back-end-PHP-MAMP\Progetto Finale\Boover-project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Desktop\Boolean\Boover-project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Desktop\Boolean\Boover-project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
