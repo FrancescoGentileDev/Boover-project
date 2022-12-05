3b3869e5b8075b0c17b10ab6c6740379023fd77d
EliGolam <elistasibusiness@gmail.com>
2022-12-06 01:39:52 +1100

    Implemented inbox factory

--- database/factories/InboxFactory.php
+++ database/factories/InboxFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\models\Inbox;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Inbox::class, function (Faker $faker) {
+    $numUsers = count(User::all());
+
     return [
-        //
+        'nickname' => $faker->firstName(),
+        'title' => $faker->realText(32),
+        'content' => $faker->realText(300),
+        'email' => $faker->safeEmail(),
+        'phone' => $faker->e164PhoneNumber(),
+        'user_id' => $faker->numberBetween(1, $numUsers),
     ];
 });
