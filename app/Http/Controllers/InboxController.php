e000b73b9d77f3830d19d16da27485be6186d9aa
EliGolam <elistasibusiness@gmail.com>
2022-12-06 00:37:23 +1100

    Changed users table migration

---
+++ app/Http/Controllers/InboxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\models\Inbox;
+use Illuminate\Http\Request;
+
+class InboxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\models\Inbox  $inbox
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Inbox $inbox)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\models\Inbox  $inbox
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Inbox $inbox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\models\Inbox  $inbox
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Inbox $inbox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\models\Inbox  $inbox
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Inbox $inbox)
+    {
+        //
+    }
+}
---
+++ database/factories/InboxFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\models\Inbox;
+use Faker\Generator as Faker;
+
+$factory->define(Inbox::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
