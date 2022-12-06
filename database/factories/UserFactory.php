4e4434bfd53a02327a39b1eb0afcecdcd8a54e71
svalvastic <assogames2@gmail.com>
2022-12-06 13:39:22 +0100

    ✨ feat(skills&user): set avatar user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-use Faker\Generator as Faker;
+use Faker\{Generator as Faker, Factory} ;
 use Illuminate\Support\Str;
 
 /*
@@ -18,9 +18,9 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-
+    $faker2 = Factory::create();
+    $faker2->addProvider(new Sabbir\Faker\AvatarProvider($faker2));
     $name = $faker->name;
-
     $days = ['M', 'T', 'W', 'Th', 'F', 'S', 'Su'];
 
     return [
@@ -30,7 +30,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
-
+        'avatar'=> $faker2->avatarUrl('personas', null, Str::slug($name)),
         'slug' => Str::slug($name),
         'phone' => $faker->e164PhoneNumber(),
         'birthday_date' => $faker->dateTimeThisCentury(),
