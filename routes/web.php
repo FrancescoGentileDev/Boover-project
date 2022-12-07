d1ce59665932af4a2f8b2966da258db414576043
svalvastic <assogames2@gmail.com>
2022-12-07 10:37:31 +0100

    ✨ feat(routes): define routes

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,17 @@ name('dashboard.')->
 prefix('dashboard')->
 group(function () {
     Route::get('/', 'DashboardController@index');
+
+    Route::get('reviews', 'ReviewController@index')->name('reviews');
+
+    Route::resource('inboxes', 'InboxController')->except(['update', 'edit']);
+
+    Route::get('profile', 'UserController@profile')->name('profile');
+    Route::post('profile', 'UserController@updateProfile')->name('profile.update');
+
+    Route::get('profile/sponsor', 'UserController@sponsor')->name('sponsor');
+    Route::post('profile/sponsor', 'UserController@addToSponsor')->name('profile.sponsor');
+
 });
 
 Route::get('{any?}', function () {
