2ecef89d0a373432f120b5132d4a7de53746bd44
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-07 11:16:20 +0100

    created stats controller

---
+++ app/Http/Controllers/Dashboard/StatisticsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StatisticsController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+    }
+}
