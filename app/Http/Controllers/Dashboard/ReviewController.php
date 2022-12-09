aa8538a5427465349ebca08007d698d4a707c97b
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-09 10:19:47 +0100

    displayed reviews in index

--- app/Http/Controllers/Dashboard/ReviewController.php
+++ app/Http/Controllers/Dashboard/ReviewController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\models\Review;
+
 class ReviewController extends Controller
 {
     //
     public function index()
     {
         //
+        $reviews = Review::all();
+        return view('dashboard.reviews.index', compact('reviews'));
     }
 }
--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    LISTA DELLE REVIEWS
+    <div class="text-3xl text-red-500">
+        Reviews List:
+    </div>
+
+    <ul>
+        @foreach ($reviews as $review)
+            <li>
+                <a href="{{ route('dashboard.reviews', $review->id) }}">{{ $review->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 @endsection
