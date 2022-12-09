98361cc9a8348dfd737e622a0c7fc02931d44d9c
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-09 10:32:24 +0100

    displayed title and author

--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-3xl text-red-500">
+    <div class="text-3xl text-red-500 mb-5">
         Reviews List:
     </div>
 
     <ul>
         @foreach ($reviews as $review)
-            <li>
-                <a href="{{ route('dashboard.reviews', $review->id) }}">{{ $review->title }}</a>
-            </li>
+            <div class="mb-4">
+                <div class="mb-1">
+                    <div class="text-green-800 text-xl">
+                        Review Title:
+                    </div>
+                    <a href="{{ route('dashboard.reviews', $review->id) }}">{{ $review->title }}</a>
+                </div>
+                <div class="mb-1">
+                    <div class="text-green-800">
+                        Author:
+                    </div>
+                    <a href="#">
+                        {{ $review->nickname }}
+                    </a>
+                </div>
+            </div>
         @endforeach
     </ul>
 @endsection
