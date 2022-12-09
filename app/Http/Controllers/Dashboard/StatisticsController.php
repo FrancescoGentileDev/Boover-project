5c8784c0d6804a8e460d294d6fb3106d20a80138
Hugocod <u.medioli@icloud.com>
2022-12-09 11:32:01 +0100

    cleaned statistics controller/view

--- app/Http/Controllers/Dashboard/StatisticsController.php
+++ app/Http/Controllers/Dashboard/StatisticsController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\models\Inbox;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 class StatisticsController extends Controller
 {
     //
     public function index()
     {
-        //
+       //
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -49740,9 +49740,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Desktop\Boolean\Boover-project\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! D:\Desktop\Boolean\Boover-project\resources\sass\styles.scss */"./resources/sass/styles.scss");
-module.exports = __webpack_require__(/*! D:\Desktop\Boolean\Boover-project\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Applications/MAMP/htdocs/php_project/PROGETTO-FINALE/Boover-project/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/php_project/PROGETTO-FINALE/Boover-project/resources/sass/styles.scss */"./resources/sass/styles.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/php_project/PROGETTO-FINALE/Boover-project/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
