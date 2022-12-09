8aa628e4cefd44add29a14f676a31b7637c2c88c
EliGolam <elistasibusiness@gmail.com>
2022-12-09 17:36:41 +1100

    Added proper user password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ $factory->define(User::class, function (Faker $faker) {
         'lastname' => $faker->lastName(),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt($faker->word()),
         'remember_token' => Str::random(10),
         'slug' => Str::slug($name),
         'phone' => $faker->e164PhoneNumber(),
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $admin = new User();
+    $admin->name = 'admin';
+    $admin->password = '$2y$10$9g6Jf09cuegp1SnkkDMdteUnLnUuL.0TJJZKykT0BsoOJcvIw2Wei';
+    $admin->email = 'admin@admin.it';
+    $admin->save();
+
+
+    $numUsers = 30;
+    $users = factory(App\User::class, $numUsers)->create();
+
+    foreach ($users as $user) {
+        $user->avatar = $this->getAvatar($user->id);
+        $user->save();
+    }
+  }
+
+  private function getAvatar($id) {
+    $gender = ['men', 'women'];
+    $baseUrl = 'https://randomuser.me/api/portraits/';
+
+    return $baseUrl . $gender[rand(0,1)] . '/'. $id . '.jpg';
+  }
+}
