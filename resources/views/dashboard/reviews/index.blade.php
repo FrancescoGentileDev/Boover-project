db6c771c7240c11054e43b6d6fb49f8a45703640
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-09 10:36:38 +0100

    displayed review content

--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-3xl text-red-500 mb-5">
+    <div class="text-3xl text-red-500 mb-5 pl-5">
         Reviews List:
     </div>
 
     <ul>
         @foreach ($reviews as $review)
-            <div class="mb-4">
+            <div class="mb-4 p-5">
                 <div class="mb-1">
                     <div class="text-green-800 text-xl">
                         Review Title:
@@ -21,6 +21,14 @@
                     <a href="#">
                         {{ $review->nickname }}
                     </a>
+                </div>
+                <div class="mb-1">
+                    <div class="text-green-800">
+                        Review:
+                    </div>
+                    <div class="w-auto">
+                        {{ $review->description }}
+                    </div>
                 </div>
             </div>
         @endforeach
