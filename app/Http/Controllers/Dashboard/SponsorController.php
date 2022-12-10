6f6681517b394878bfdb94b7ea08123731723e72
Hugocod <u.medioli@icloud.com>
2022-12-10 12:02:45 +0100

    added methods Sponsor $ addToSponsor

--- app/Http/Controllers/Dashboard/SponsorController.php
+++ app/Http/Controllers/Dashboard/SponsorController.php
@@ -1,14 +1,29 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use App\models\Sponsor;
+use Illuminate\Support\Facades\Auth;
 
 class SponsorController extends Controller
 {
-    //
-    public function index()
+
+    public function sponsor()
     {
         //
+        $user = Auth::user();
+        $sponsors = Sponsor::all();
+
+        return view('dashboard.profile.sponsor', compact('sponsors'));
     }
+
+    public function addToSponsor(Request $request)
+    {
+        //
+        dd($request->all());
+    }
+
+
 }
