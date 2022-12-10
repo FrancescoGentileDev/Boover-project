477c117708d76e9198bf77997d8fa9dfb8c94a1d
Hugocod <u.medioli@icloud.com>
2022-12-10 15:56:26 +0100

    update addToSponsor + validation

--- app/Http/Controllers/Dashboard/SponsorController.php
+++ app/Http/Controllers/Dashboard/SponsorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use App\models\Sponsor;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class SponsorController extends Controller
 {
@@ -22,7 +23,29 @@ class SponsorController extends Controller
     public function addToSponsor(Request $request)
     {
         //
-        dd($request->all());
+
+        $logged = Auth::user()->id;
+        $user = User::find($logged);
+        $sponsorsRecords = DB::table('sponsor_user')->where( 'user_id','=', $logged )->orderByDesc('created_at')->first() ;
+
+         if ($sponsorsRecords->expire_date > date('Y-m-d H:i:s') ) {
+            # code...
+            return redirect()->route('dashboard.profile.sponsor.store')->with('justsponsor','sponzorizzazione fallita');
+        };
+
+
+
+        $request->validate([
+            'sponsor' => ['required', 'exists:sponsors,id']
+        ]);
+
+
+        $sponsor = Sponsor::find($request->sponsor);
+
+        $newDate = date("Y-m-d H:i:s", strtotime("+{$sponsor->duration} hours"));
+        $user->sponsors()->attach($sponsor->id, ['expire_date' => $newDate, 'created_at' => date('Y-m-d H:i:s')]);
+
+        return redirect()->route('dashboard.profile.sponsor.store')->with('success','Sponsorizzazione aggiunta con successo!');
     }
 
 
