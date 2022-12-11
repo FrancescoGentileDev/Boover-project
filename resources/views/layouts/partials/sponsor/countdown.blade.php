72250afd44ac6a0410a895c5e5766d6f98847232
Hugocod <u.medioli@icloud.com>
2022-12-11 13:51:42 +0100

    added countdown component

---
+++ resources/views/layouts/partials/sponsor/countdown.blade.php
@@ -0,0 +1,34 @@
+
+<h1 class="text-center" id="end">Il tuo boost scade tra...</h1>
+<div class="flex justify-center text-center">
+
+    <div class="flex flex-col align-center justify-center">
+      <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
+        <span id="days"  style="--value:15;"></span>
+      </span>
+      days
+    </div>
+
+    <div class="flex flex-col">
+      <span class=" p-2  countdown font-mono text-4xl md:text-7xl ">
+        <span id="hours" style="--value:10;"></span>
+      </span>
+      hours
+    </div>
+
+    <div class="flex flex-col">
+      <span class=" p-2  countdown font-mono text-4xl md:text-7xl">
+        <span id="mins" style="--value:24;"></span>
+      </span>
+      min
+    </div>
+
+    <div class="flex flex-col">
+      <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
+        <span id="secs" style="--value:22;"></span>
+      </span>
+      sec
+    </div>
+</div>
+
+
