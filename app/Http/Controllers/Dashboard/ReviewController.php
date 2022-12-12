f5be70a8a5d263bb735afd1fd46340bc649da661
svalvastic <assogames2@gmail.com>
2022-12-12 02:29:19 +0100

    🐞 fix(reviewController): show all review fix

--- app/Http/Controllers/Dashboard/ReviewController.php
+++ app/Http/Controllers/Dashboard/ReviewController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 use App\models\Review;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
@@ -13,7 +16,10 @@ class ReviewController extends Controller
     public function index()
     {
         //
-        $reviews = Review::paginate(10);
+        $logged = Auth::user()->id;
+
+        $reviews = Review::where('user_id', $logged)->paginate(10);       
+        
         return view('dashboard.reviews.index', compact('reviews'));
     }
 
