49c46fdc8c5bcf0b85a498a98cc118ebc8154305
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-12 14:54:32 +0100

    fixed inbox layout

--- resources/views/dashboard/inboxes/index.blade.php
+++ resources/views/dashboard/inboxes/index.blade.php
@@ -1,25 +1,19 @@
-@extends('layouts.app-emg',
-  [
-    'title' => 'Boover | My Inbox',
-    'pageTitle' => 'My Inbox',
-  ]
-)
+@extends('layouts.app')
 
 @section('content')
-  <h2 class="text-lg text-center">Your Inbox</h2>
+    <h2 class="text-lg text-center">Your Inbox</h2>
 
-  <ul id="inbox-list" class="w-full md:w-3/4 flex flex-col gap-3 mx-auto mt-4 mb-8">
-    @foreach ($inboxes as $inbox)
-      <li class="bg-purple-400 p-2 rounded
-        hover:bg-purple-300"
-      >
-        <a href="{{ route('dashboard.inboxes.show', $inbox->id) }}">
-          <p>{{ $inbox->title}}</p>
-          <p><em class="text-white text-sm">by {{ $inbox->nickname }}</em></p>
-        </a>
-      </li>
-    @endforeach
-  </ul>
+    <ul id="inbox-list" class="w-full md:w-3/4 flex flex-col gap-3 mx-auto mt-4 mb-8">
+        @foreach ($inboxes as $inbox)
+            <li class="bg-purple-400 p-2 rounded
+        hover:bg-purple-300">
+                <a href="{{ route('dashboard.inboxes.show', $inbox->id) }}">
+                    <p>{{ $inbox->title }}</p>
+                    <p><em class="text-white text-sm">by {{ $inbox->nickname }}</em></p>
+                </a>
+            </li>
+        @endforeach
+    </ul>
 
-  {{-- @dump($inboxes) --}}
+    {{-- @dump($inboxes) --}}
 @endsection
