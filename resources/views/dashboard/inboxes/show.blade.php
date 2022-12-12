6be39034df4cccab1535641724d48a9ec001cb3e
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-12 15:06:47 +0100

    fixed extended layout + small css fixing

--- resources/views/dashboard/inboxes/show.blade.php
+++ resources/views/dashboard/inboxes/show.blade.php
@@ -1,25 +1,20 @@
-@extends('layouts.app-emg',
-  [
-    'title' => 'Boover | My Inbox',
-    'pageTitle' => 'My Inbox',
-  ]
-)
+@extends('layouts.app')
 
 @section('content')
-  <article class="w-3/4 mx-auto my-5">
-    <h3 class="text-lg text-purple-500">{{ $inbox->title }}</h3>
-    <p class="text-gray-600">by {{ $inbox->nickname }}</p>
-    <p class="my-5">{{ $inbox->content }}</p>
+    <article class="w-3/4 mx-auto my-7">
+        <h3 class="text-lg text-purple-500"><span class="text-red-500">Message Title: </span>{{ $inbox->title }}</h3>
+        <p class="text-gray-600">by {{ $inbox->nickname }}</p>
+        <p class="my-5">{{ $inbox->content }}</p>
 
-    <section class="text-sm text-gray-600 my-4">
-      <h4 class="text-purple-400">Contacts</h4>
+        <section class="text-sm text-gray-600 my-4">
+            <h4 class="text-purple-400">Contacts</h4>
 
-      <p>Phone: {{ $inbox->phone }}</p>
-      <p>Email: {{ $inbox->email }}</p>
-    </section>
+            <p>Phone: {{ $inbox->phone }}</p>
+            <p>Email: {{ $inbox->email }}</p>
+        </section>
 
-    <x-delete-form :destroyRoute="route('dashboard.inboxes.destroy', $inbox->id)" />
-  </article>
+        <x-delete-form :destroyRoute="route('dashboard.inboxes.destroy', $inbox->id)" />
+    </article>
 
-  {{-- @dump($inbox) --}}
+    {{-- @dump($inbox) --}}
 @endsection
