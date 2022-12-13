403dca0ea8b4f1cc5d5b4871e0466f5c4ea0a4a3
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-13 14:36:16 +0100

    added example contents

--- resources/js/sections/HomeHeroSection.vue
+++ resources/js/sections/HomeHeroSection.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="hero-component" class="py-10">
-    <div class="container flex flex-row">
-      <div class="w-2/4">LEFT SIDE</div>
+    <div class="container flex flex-row py-10">
+      <div class="w-2/4">
+        <div class="text-2xl mb-5">Titolo di questa Hero</div>
+        <div id="contents" class="flex flex-col gap-y-2">
+          <div>
+            <div>Content Title 1</div>
+            <p>Description 1</p>
+          </div>
+          <div>
+            <div>Content Title 2</div>
+            <p>Description 2</p>
+          </div>
+          <div>
+            <div>Content Title 3</div>
+            <p>Description 3</p>
+          </div>
+        </div>
+      </div>
       <div class="w-2/4">RIGHT SIDE</div>
     </div>
   </div>
