8ee27996da281fe90fd66231cb2276071e59caa6
Hugocod <u.medioli@icloud.com>
2022-12-13 17:53:05 +0100

    added testimonialsSection

---
+++ resources/js/sections/TestimonialsSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrapper mt-96">
+        <!--  // -->
+
+        <div class="flex w-4/5 m-auto gap-9 flex-col md:flex-row">
+            <div class="aspect-[4/2] md:w-2/4 w-full bg-slate-400">
+                <img
+                    src="https://placeimg.com/800/400/people"
+                    class="w-full h-full object-cover rounded-lg shadow-2xl"
+                />
+            </div>
+            <div class="md:w-2/4 w-full flex flex-col justify-center gap-2">
+                <h4>John doe, Chief Commercial Officier | NAADA</h4>
+                <q class="text-3xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Voluptate itaque eos veritatis illo consequuntur dicta ad
+                    soluta expedita officiis distinctio labore maiores placeat
+                    minus, corporis quis porro rerum, a autem.
+                </q>
+            </div>
+        </div>
+
+        <!--  // -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TestimonialsSection",
+};
+</script>
+
+<style></style>
