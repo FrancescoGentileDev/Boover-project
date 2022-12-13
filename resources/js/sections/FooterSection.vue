e5822f2ae1b8f464672e35c2e59b866a4b28c641
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-13 15:21:13 +0100

    created footer vue and imported

---
+++ resources/js/sections/FooterSection.vue
@@ -0,0 +1,10 @@
+!<template>
+  <div>FOOTER</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
+    <footer-section />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import FooterSection from "../sections/FooterSection.vue";
 import homeView from "./homeView.vue";
 export default {
-  components: { homeView },
+  components: { homeView, FooterSection },
 };
 </script>
 
