221b5abff6f8c35242cb514a452b3655f8c18b62
svalvastic <assogames2@gmail.com>
2022-12-13 10:48:03 +0100

    🎉 init(vue): init vue frontOffice

---
+++ resources/js/views/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import homeSection from './homeSection.vue';
+export default {
+  components: { homeSection },};
+</script>
+
+<style></style>
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Document</title>
-</head>
-
-<body>
-    <p class=" text-red-500">GUEST</p>
-
-    <a class="inline-flex px-4 py-2 my-2 text-sm font-medium bg-white rounded-md shadow-sm hover:bg-gray-50" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Link with href</a>
-<button class="inline-flex px-4 py-2 my-2 text-sm font-medium text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-700" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Button with data-bs-target</button>
-
-<div class="collapse" id="collapseExample">
-  <div class="p-4 bg-white rounded">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</div>
-</div>
-</body>
-
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>Boolpress</title>
+    </head>
+    <body>
+      <div id="app"></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+    <style>
+        body {
+            background-color: rgb(235, 235, 235)
+        }
+    </style>
 </html>
