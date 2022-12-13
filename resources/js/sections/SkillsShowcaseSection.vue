a92af605943a387430b415f6f278fac64d06cabc
Hugocod <u.medioli@icloud.com>
2022-12-13 18:59:11 +0100

    added basic structure

---
+++ resources/js/sections/SkillsShowcaseSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper pt-24 pb-24">
+        <!--  // -->
+
+        <!--  // -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SkillsShowcaseSection",
+};
+</script>
+
+<style></style>
