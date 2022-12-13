7c50e6781377c6ff8bea034aace7cdc922efeeb9
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-13 15:44:10 +0100

    removed footer debug from app

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <footer-section />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import FooterSection from "../sections/FooterSection.vue";
 import homeView from "./homeView.vue";
 export default {
-  components: { homeView, FooterSection },
+  components: { homeView },
 };
 </script>
 
