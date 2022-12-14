f906cf149ef3a03d421b127c7bbf71686b29560f
svalvastic <assogames2@gmail.com>
2022-12-14 12:04:55 +0100

    ✨ feat(app.vue): dynamic pad header

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,9 +1,8 @@
 <template>
-  <div data-theme="light">
-    <NavbarComponent />
-    <router-view class="pt-[8.75rem]"></router-view>
-    <footer-section />
-    test
+  <div data-theme="light" class="relative">
+    <NavbarComponent ref="navbar"/>
+    <router-view ref="content" class="min-h-[100vh]"></router-view>
+    <footer-section class="bottom-0"/>
   </div>
 </template>
 
@@ -12,6 +11,14 @@ import NavbarComponent from "../components/NavbarComponent.vue";
 import FooterSection from "../sections/FooterSection.vue";
 export default {
   components: { NavbarComponent, FooterSection },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.content.$el.style.paddingTop = this.$refs.navbar.$el.offsetHeight + 'px';
+    }, 1000)
+    window.addEventListener('resize', () => {
+      this.$refs.content.$el.style.paddingTop = this.$refs.navbar.$el.offsetHeight + 'px';
+    })
+  }
 };
 </script>
 
