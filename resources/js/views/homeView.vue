ee60ea807a298575c09df23659842a837b0362c9
Hugocod <u.medioli@icloud.com>
2022-12-15 19:06:24 +0100

    added testimonialsSection component into homeView

--- resources/js/views/homeView.vue
+++ resources/js/views/homeView.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
-        <main-hero-section :categories="categories"/>
+        <main-hero-section :categories="categories" />
         <skills-showcase-section></skills-showcase-section>
+        <testimonials-section></testimonials-section>
     </div>
 </template>
 
@@ -9,10 +10,16 @@
 import HomeHeroSection from "../sections/HomeHeroSection.vue";
 import MainHeroSection from "../sections/mainHeroSection.vue";
 import SkillsShowcaseSection from "../sections/SkillsShowcaseSection.vue";
+import TestimonialsSection from "../sections/TestimonialsSection.vue";
 export default {
     name: "HomeView",
 
-    components: { SkillsShowcaseSection, HomeHeroSection, MainHeroSection },
+    components: {
+        SkillsShowcaseSection,
+        HomeHeroSection,
+        MainHeroSection,
+        TestimonialsSection,
+    },
     props: {
         categories: {
             type: Array,
