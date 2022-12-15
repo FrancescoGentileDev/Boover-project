a4d04ae250fe0d2477d06c374ec4034fa9993932
Hugocod <u.medioli@icloud.com>
2022-12-16 00:11:26 +0100

    added jumbo basic structure

---
+++ resources/js/sections/jumbotronSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper pt-24 pb-24 bg-base-100"></div>
+</template>
+
+<script>
+export default {
+    name: "jumbotronSection",
+    //
+    data() {
+        return {
+            pics: [
+                /*   "https://images.pexels.com/photos/5407215/pexels-photo-5407215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", */
+            ],
+        };
+    },
+};
+</script>
+
+<style></style>
