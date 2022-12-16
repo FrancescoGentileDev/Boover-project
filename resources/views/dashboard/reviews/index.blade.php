efdd08ce819114c43709eb13758ab7ed8e7bd715
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 09:59:41 +0100

    created new review skin in back end

--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -11,31 +11,31 @@
 
     <div class="px-5">
         @foreach ($reviews as $review)
-            <div class="mb-4 p-5 border-2">
-                <div class="mb-1">
-                    <div class="text-green-800 text-xl">
-                        Review Title:
-                    </div>
-                    <a href="{{ route('dashboard.reviews', $review->id) }}">{{ $review->title }}</a>
+            <div class="max-w-2xl px-8 py-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+                <div class="flex items-center justify-between">
+                    <span class="text-sm font-light text-gray-600 dark:text-gray-400">{{ $review->created_at }}</span>
                 </div>
-                <div class="mb-1">
-                    <div class="text-green-800">
-                        Author:
-                    </div>
-                    <a href="#">
-                        {{ $review->nickname }}
-                    </a>
+
+                <div class="mt-2">
+                    <a href="#"
+                        class="text-2xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline"
+                        tabindex="0" role="link">{{ $review->title }}</a>
+                    <p class="mt-2 text-gray-600 dark:text-gray-300">
+                        {{ $review->description }}</p>
                 </div>
-                <div class="mb-1">
-                    <div class="text-green-800">
-                        Review:
-                    </div>
-                    <div>
-                        <span>Vote: </span> <span>{{ $review->vote }}</span><span> | rilasciata il:
-                        </span><span>{{ $review->created_at }}</span>
-                    </div>
-                    <div class="w-auto">
-                        {{ $review->description }}
+
+                <div class="flex items-center justify-between mt-4">
+                    <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline" tabindex="0"
+                        role="link">Read
+                        more</a>
+
+                    <div class="flex items-center">
+                        <img class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block"
+                            src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40&q=80"
+                            alt="avatar">
+                        <a class="font-bold text-gray-700 cursor-pointer dark:text-gray-200" tabindex="0"
+                            role="link">Khatab
+                            wedaa</a>
                     </div>
                 </div>
             </div>
