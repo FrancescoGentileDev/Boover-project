14ada918f788656294c1ee7b976c841158b93082
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 12:23:53 +0100

    fixed color in inbox show

--- resources/views/dashboard/inboxes/show.blade.php
+++ resources/views/dashboard/inboxes/show.blade.php
@@ -5,19 +5,20 @@
 @section('content')
     <article style="padding-top: 10%" class="w-3/4 mx-auto my-7">
         <div class="flex items-center justify-between">
-            <span class="text-sm font-light text-gray-600">{{ $inbox->created_at }}</span>
+            <span class="text-sm font-light text-neutral">{{ $inbox->created_at }}</span>
         </div>
 
         <div class="mt-2">
-            <div class="text-2xl font-bold text-gray-700" tabindex="0" role="link">Message Title: {{ $inbox->title }}
+            <div class="text-2xl font-bold text-neutral" tabindex="0" role="link">Message Title: {{ $inbox->title }}
             </div>
-            <p class="text-gray-600">From: {{ $inbox->nickname }}</p>
-            <p class="mt-2 text-gray-600">{{ $inbox->content }}</p>
+            <p class="text-gray-600"><span class="text-neutral font-semibold">From: </span> {{ $inbox->nickname }}</p>
+            <p class="mt-2 text-gray-600"><span class="text-neutral font-semibold">Message: </span>
+                {{ $inbox->content }}</p>
         </div>
 
         <div class="flex items-center justify-between mt-4">
-            <div class="text-sm text-gray-600 my-4">
-                <h4 class="text-base-content">Contacts:</h4>
+            <div class="text-sm my-4">
+                <h4 class="text-neutral font-semibold">Contacts:</h4>
 
                 <p>Phone: {{ $inbox->phone }}</p>
                 <p>Email: {{ $inbox->email }}</p>
