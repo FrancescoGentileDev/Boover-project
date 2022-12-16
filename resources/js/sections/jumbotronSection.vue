b1de8d3af840ebfe53f322981836f8a1c9bf99c7
Hugocod <u.medioli@icloud.com>
2022-12-16 03:09:02 +0100

    completed jumbotron

--- resources/js/sections/jumbotronSection.vue
+++ resources/js/sections/jumbotronSection.vue
@@ -1,5 +1,28 @@
 <template>
-    <div class="wrapper pt-24 pb-24 bg-base-100"></div>
+    <div class="wrapper pt-24 pb-24">
+        <div
+            class="bg-primary flex justify-center align-center m-auto w-11/12 p-24 relative rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 .."
+        >
+            <!-- // -->
+            <div class="">
+                <h1 class="text-5xl w-full lg:w-3/4 font-normal text-white">
+                    Find the
+                    <span class="font-bold">talent</span> needed to get your
+                    business <span class="font-extrabold">growing.</span>
+                </h1>
+                <button class="btn btn-secondary mt-4">Get Started</button>
+            </div>
+            <!-- // -->
+            <div class="lg:w-4/6">
+                <img
+                    src="https://res.cloudinary.com/dflyrzhj9/image/upload/v1671149669/waist-up-shot-attractive-young-girlfriend-with-dark-straight-hair-soft-healthy-skin_k1xjd7.png"
+                    alt=""
+                    class="invisible lg:visible absolute bottom-0 right-0"
+                />
+            </div>
+            <!-- // -->
+        </div>
+    </div>
 </template>
 
 <script>
