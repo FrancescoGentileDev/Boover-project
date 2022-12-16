e5b85c75d2fc5c6931a16d06652cdfaed9c83515
Carmyo <carmelodicristina@gmail.com>
2022-12-16 17:22:15 +0100

    adjust flex in reviews ssection

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -242,30 +242,31 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="container px-6 py-10 mx-auto">
         <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">
-            What our <span class="text-blue-500 ">clients</span> say
+            What our <span class="text-secondary ">clients</span> say
         </h1>
 
         <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error
             alias, adipisci rem similique, at omnis eligendi optio eos harum.
         </p>
+       <div class="grid grid-cols-1 gap-8 mx-auto mt-8 lg:grid-cols-2 xl:mt-10 max-w-7xl">
+           <div v-for="(reviews,index) in reviews" :key="index" >
+               <div class="p-6 bg-gray-100 rounded-lg dark:bg-gray-800 md:p-8">
+                   <p class="leading-loose text-gray-500 dark:text-gray-300">
+                       “{{reviews.review}}.”.
+                   </p>
 
-        <div v-for="(reviews,index) in reviews" :key="index" class="grid grid-cols-1 gap-8 mx-auto mt-8 lg:grid-cols-2 xl:mt-10 max-w-7xl">
-            <div class="p-6 bg-gray-100 rounded-lg dark:bg-gray-800 md:p-8">
-                <p class="leading-loose text-gray-500 dark:text-gray-300">
-                    “Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum, obcaecati corrupti aspernatur a.”.
-                </p>
-
-                <div class="flex items-center mt-6">
-                    <img class="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
+                   <div class="flex items-center mt-6">
+                       <img class="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
                     
-                    <div class="mx-4">
-                        <h1 class="font-semibold text-blue-500">Robbert</h1>
-                        <span class="text-sm text-gray-500 dark:text-gray-300">{{reviews.profession}}, {{reviews.userName}}</span>
-                    </div>
-                </div>
-            </div>            
-        </div>
+                       <div class="mx-4">
+                           <h1 class="font-semibold text-blue-500">{{reviews.userName}}</h1>
+                           <span class="text-sm text-gray-500 dark:text-gray-300">{{reviews.profession}}</span>
+                       </div>
+                   </div>
+               </div>            
+           </div>
+       </div>
     </div>
 </section>
 
@@ -298,14 +299,6 @@
         </div>
      </div>
     </section>
-
-
-
-
-
-
-    
-
     <!--REGISTER-->
     <section class="register">
       <div class="text-center">
@@ -353,13 +346,24 @@ export default {
                 profession:"Professional",
             },
             {
-                review:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, quam. Odio voluptatem officiis eos illo! Pariatur, totam alias. Beatae accusamus earum quos obcaecati minima molestias. Possimus minima dolores itaque! Esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptates fugiat corrupti laudantium dolores reiciendis pariatur esse quod nihil quia cupiditate debitis quisquam nemo, accusamus animi explicabo? Architecto, unde laboriosam?",
+                review:"Lorem ipsum dolor sit amet, consectetur abo? Architecto, unde laboriosam?",
                 propic:"image",
-                userName:"Name Surname",
-                profession:"Professional",
+                userName:"Tizio Di Caio",
+                profession:"Researcher",
             },
+            {
+                review:"Lorem ipsum dolor sit amet,Aperiam, quam. Odio voluptatas. Possimus minima dolores itaque! Esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptates fugiat corrupti laudantium dolores reiciendis pariatur esse quod nihil quia cupiditate debitis quisquam nemo, accusamus animi explicabo? Architecto, unde laboriosam?",
+                propic:"image",
+                userName:"Caio Di Tizio",
+                profession:"Developer",
+            },
+            {
+                review:"Lorem ipsum dolor sit amet, voluptatem officiis eos illo! Pariatur, totam alias. Beatae accusamus earum quos obcaecati minima molestias. Possimus minima dolores itaque! Esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptates fugiat corrupti laudantium dolores reiciendis pariatur esse quod nihil quia cupiditate debitis quisquam nemo, accusamus animi explicabo? Architecto, unde laboriosam?",
+                propic:"image",
+                userName:"Mario Rossi",
+                profession:"Student",
+            },            
         ]
-
     }),
 
 mounted() {
@@ -370,8 +374,5 @@ mounted() {
     },
 }
 </script>
-
 <style lang="scss" scoped>
-
-
 </style>
