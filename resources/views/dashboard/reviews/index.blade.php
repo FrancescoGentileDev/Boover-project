c7d9451d072c52a8be166324882fdc1f3ed242d4
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 10:30:46 +0100

    better text color in dark theme

--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -17,24 +17,24 @@
         @foreach ($reviews as $review)
             <div class="my-7 px-8 py-4 bg-base-200 rounded-lg shadow-md dark:bg-gray-800">
                 <div class="flex items-center justify-between">
-                    <span class="text-sm font-light text-gray-600 dark:text-gray-400">{{ $review->created_at }}</span>
+                    <span class="text-sm font-light text-base-content">{{ $review->created_at }}</span>
                 </div>
 
                 <div class="mt-2">
                     <a href="#"
-                        class="text-2xl font-bold text-gray-700 dark:text-white hover:text-gray-600 dark:hover:text-gray-200 hover:underline"
+                        class="text-2xl font-bold text-base-content hover:text-gray-600 dark:hover:text-gray-200 hover:underline"
                         tabindex="0" role="link">{{ $review->title }}</a>
-                    <p class="mt-2 text-gray-600 dark:text-gray-300">
+                    <p class="mt-2 text-base-content">
                         {{ $review->description }}</p>
                 </div>
 
                 <div class="flex items-center justify-between mt-4">
                     <div class="flex items-center">
-                        <div class="font-bold text-gray-700 dark:text-gray-200" tabindex="0" role="link">
+                        <div class="font-bold text-base-content" tabindex="0" role="link">
                             Valutation: {{ $review->vote }}</div>
                     </div>
                     <div class="flex items-center">
-                        <div class="font-bold text-gray-700 dark:text-gray-200" tabindex="0" role="link">
+                        <div class="font-bold text-base-content" tabindex="0" role="link">
                             {{ $review->nickname }}</div>
                     </div>
                 </div>
