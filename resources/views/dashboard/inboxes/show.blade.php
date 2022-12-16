ca758eb1fd1f9447e315b2666214c58b442694ad
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 12:08:59 +0100

    added better inbox card style

--- resources/views/dashboard/inboxes/show.blade.php
+++ resources/views/dashboard/inboxes/show.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.app', [
-  'title' => 'Boover | Inbox'
+    'title' => 'Boover | Inbox',
 ])
 
 @section('content')
     <article class="w-3/4 mx-auto my-7">
-        <h3 class="text-lg text-purple-500"><span class="text-red-500">Message Title: </span>{{ $inbox->title }}</h3>
-        <p class="text-gray-600">by {{ $inbox->nickname }}</p>
-        <p class="my-5">{{ $inbox->content }}</p>
+        <div class="flex items-center justify-between">
+            <span class="text-sm font-light text-gray-600">{{ $inbox->created_at }}</span>
+        </div>
 
-        <section class="text-sm text-gray-600 my-4">
-            <h4 class="text-purple-400">Contacts</h4>
+        <div class="mt-2">
+            <div class="text-2xl font-bold text-gray-700" tabindex="0" role="link">Message Title: {{ $inbox->title }}
+            </div>
+            <p class="text-gray-600">From: {{ $inbox->nickname }}</p>
+            <p class="mt-2 text-gray-600">{{ $inbox->content }}</p>
+        </div>
 
-            <p>Phone: {{ $inbox->phone }}</p>
-            <p>Email: {{ $inbox->email }}</p>
-        </section>
+        <div class="flex items-center justify-between mt-4">
+            <div class="text-sm text-gray-600 my-4">
+                <h4 class="text-base-content">Contacts:</h4>
 
+                <p>Phone: {{ $inbox->phone }}</p>
+                <p>Email: {{ $inbox->email }}</p>
+            </div>
+        </div>
         <x-delete-form :destroyRoute="route('dashboard.inboxes.destroy', $inbox->id)" />
     </article>
 
