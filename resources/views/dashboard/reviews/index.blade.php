627118c1712602f0488fd904cae4bde5268631c2
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 10:02:23 +0100

    added review displayed infos

--- resources/views/dashboard/reviews/index.blade.php
+++ resources/views/dashboard/reviews/index.blade.php
@@ -25,17 +25,9 @@
                 </div>
 
                 <div class="flex items-center justify-between mt-4">
-                    <a href="#" class="text-blue-600 dark:text-blue-400 hover:underline" tabindex="0"
-                        role="link">Read
-                        more</a>
-
                     <div class="flex items-center">
-                        <img class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block"
-                            src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=40&q=80"
-                            alt="avatar">
                         <a class="font-bold text-gray-700 cursor-pointer dark:text-gray-200" tabindex="0"
-                            role="link">Khatab
-                            wedaa</a>
+                            role="link">{{ $review->nickname }}</a>
                     </div>
                 </div>
             </div>
