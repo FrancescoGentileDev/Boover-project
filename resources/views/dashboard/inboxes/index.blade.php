cee136012f02e033ab40af1af9b2a58da7c6d07e
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-16 10:41:51 +0100

    bette inboxes colors

--- resources/views/dashboard/inboxes/index.blade.php
+++ resources/views/dashboard/inboxes/index.blade.php
@@ -3,8 +3,11 @@
 ])
 
 @section('content')
-    <div class="text-3xl text-center my-7">
-        Your Inbox
+    <div class="flex flex-row justify-center">
+        <div
+            class="basis-2/4 text-center my-7 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg">
+            Your Inbox
+        </div>
     </div>
 
     <div class="mb-5">
@@ -13,11 +16,11 @@
 
     <ul id="inbox-list" class="w-full md:w-3/4 flex flex-col gap-3 mx-auto mt-4 mb-8">
         @foreach ($inboxes as $inbox)
-            <li class="bg-blue-500 p-2 rounded
-        hover:bg-purple-300">
+            <li class="px-5 py-2 bg-base-200 rounded-lg shadow-md dark:bg-gray-800
+        hover:bg-base-300">
                 <a href="{{ route('dashboard.inboxes.show', $inbox->id) }}">
                     <p>{{ $inbox->title }}</p>
-                    <p><em class="text-white text-sm">by {{ $inbox->nickname }}</em></p>
+                    <p><em class="text-base-content text-sm">From: {{ $inbox->nickname }}</em></p>
                 </a>
             </li>
         @endforeach
