d6f9deeec09f2ae19de7a4245ba690b19d49b67e
Carmyo <carmelodicristina@gmail.com>
2022-12-17 12:34:34 +0100

    change h1 color in team section

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -94,11 +94,11 @@
 
                                                   <!--TEAM-->
 
-    <section class="bg-white dark:bg-gray-900">
+    <section class="bg-white">
     <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">our team</h1>
+        <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl">Conosci i nostri migliori <span class="text-secondary">Professionisti</span></h1>
 
-        <p class="max-w-2xl mx-auto my-6 text-center text-gray-500 dark:text-gray-300">
+        <p class="max-w-2xl mx-auto my-6 text-center text-gray-500">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
         </p>
                                                 
