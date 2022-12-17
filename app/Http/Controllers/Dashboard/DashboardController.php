9a2e465a6bc98156989debac68ac241364937d75
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-17 10:07:18 +0100

    added auth user in dashboard controller

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
-    public function index() {
-        return view('dashboard.index');
+    public function index()
+    {
+        $logged = Auth::user();
+        return view('dashboard.index', compact('logged'));
     }
-
 }
