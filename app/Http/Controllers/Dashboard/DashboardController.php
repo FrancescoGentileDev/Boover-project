aecaa493bfe585a93d2455c0caa9028ae1f177fc
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-17 13:18:53 +0100

    found inboxes paginated(5) in controller dash

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\models\Review;
+use App\models\Inbox;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
@@ -17,6 +18,7 @@ class DashboardController extends Controller
         $logged = Auth::user()->id;
         $user = User::find($logged);
         $reviews = Review::where('user_id', $logged)->orderBy('created_at', 'desc')->paginate(5);
+        $inboxes = Inbox::where('user_id', Auth::id())->orderBy('created_at', 'desc')->paginate(5);
 
         // reviews and inboxes
         $totalReviews = $user->reviews()->count();
@@ -28,6 +30,6 @@ class DashboardController extends Controller
         // vote average
         $average = $user->reviews()->avg('vote');
 
-        return view('dashboard.index', compact('user', 'totalReviews', 'totalInboxes', 'isSponsor', 'average', 'reviews'));
+        return view('dashboard.index', compact('user', 'totalReviews', 'totalInboxes', 'isSponsor', 'average', 'reviews', 'inboxes'));
     }
 }
