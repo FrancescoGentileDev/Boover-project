a43ecd76b4710020f152c19e10f211f85575c09b
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-17 12:58:19 +0100

    created review show layout

--- resources/views/dashboard/reviews/show.blade.php
+++ resources/views/dashboard/reviews/show.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        TEST
+    <div class="py-10">
+        <div class="my-7 px-8 py-4 bg-base-200 rounded-lg shadow-md dark:bg-gray-800 hover:bg-base-300">
+            <div class="flex items-center justify-between">
+                <span class="text-sm font-light text-base-content">{{ $review->created_at }}</span>
+            </div>
+
+            <div class="mt-2">
+                <div class="text-2xl font-bold text-base-content" tabindex="0" role="link">
+                    {{ $review->title }}</div>
+                <p class="mt-2 text-base-content">
+                    {{ $review->description }}</p>
+            </div>
+
+            <div class="flex items-center justify-between mt-4">
+                <div class="flex items-center">
+                    <div class="font-bold text-base-content" tabindex="0" role="link">
+                        Valutation: {{ $review->vote }}</div>
+                </div>
+                <div class="flex items-center">
+                    <div class="font-bold text-base-content" tabindex="0" role="link">
+                        {{ $review->nickname }}</div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
