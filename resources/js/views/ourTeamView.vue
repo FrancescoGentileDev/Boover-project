14b3e06702bcebf981895b1691012e492c59a6bc
Carmyo <carmelodicristina@gmail.com>
2022-12-17 11:18:21 +0100

    change color name in reviews

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -94,10 +94,10 @@
                                  
                                          
                                          <!--REVIEW CUSTOM 2-->
-<section class="bg-white dark:bg-gray-900">
+<section class="bg-base-200 dark:bg-gray-900"> 
     <div class="container px-6 py-10 mx-auto">
         <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">
-            What our <span class="text-secondary ">clients</span> say
+            What our <span class="text-secondary">clients</span> say
         </h1>
 
         <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
@@ -115,7 +115,7 @@
                        <img class="object-cover rounded-full w-14 h-14" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
                     
                        <div class="mx-4">
-                           <h1 class="font-semibold text-blue-500">{{reviews.userName}}</h1>
+                           <h1 class="font-semibold text-secondary">{{reviews.userName}}</h1>
                            <span class="text-sm text-gray-500 dark:text-gray-300">{{reviews.profession}}</span>
                        </div>
                    </div>
