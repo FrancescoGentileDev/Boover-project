45fd42a1ecc028187d05944077e31f57d175f18d
svalvastic <assogames2@gmail.com>
2022-12-18 15:43:47 +0100

    ✨ feat(filter): add starsystem

--- resources/js/components/filterComponent.vue
+++ resources/js/components/filterComponent.vue
@@ -1,75 +1,99 @@
 <template>
-    <form @submit="applyFilters" class="flex gap-9 items-end justify-end">
-        <div class="form-control">
+  <div>
+    <button @click="isCollapsed = !isCollapsed" class="btn btn-md btn-accent">
+      Filtri avanzati
+    </button>
+    <transition name="slide" >
+      <div v-show="isCollapsed">
+        <form @submit="applyFilters" class="flex flex-col sm:flex-row gap-9 items-end justify-end">
+          <div class="form-control">
             <label class="label cursor-pointer p-0">
-                <span class="label-text px-3">Solo Choice</span>
-                <input
-                    name="only_sponsorized"
-                    type="checkbox"
-                    class="toggle toggle-primary"
-                    v-model="onlySponsor"
-                />
+              <span class="label-text px-3">Solo Choice</span>
+              <input
+                name="only_sponsorized"
+                type="checkbox"
+                class="toggle toggle-primary"
+                v-model="onlySponsor"
+              />
             </label>
-        </div>
-        <div class="form-control">
+          </div>
+          <div class="form-control">
             <label class="label cursor-pointer p-0">
-                <span class="label-text px-3">I più recensiti</span>
-                <input
-                    name="most_reviewed"
-                    type="checkbox"
-                    class="toggle toggle-secondary"
-                    v-model="mostReviewed"
-
-                />
+              <span class="label-text px-3">I più recensiti</span>
+              <input
+                name="most_reviewed"
+                type="checkbox"
+                class="toggle toggle-secondary"
+                v-model="mostReviewed"
+              />
             </label>
-        </div>
+          </div>
 
-        <div class="flex flex-col">
+          <div class="flex flex-col items-center gap-2">
             <label for="rating-min">Range media recensioni</label>
-            <div class="rating rating-md flex flex-row-reverse justify-center">
-                <input
-                    ref="ratingMin"
-                    v-for="i in 5"
-                    :key="i"
-                    type="radio"
-                    :value="5 - i + 1"
-                    name="rating-min"
-                    class="mask mask-star-2 bg-orange-400"
-                />
+            <div class="flex ">
+              <star-system-component @ratingRange="ratingEvent"/>
             </div>
-        </div>
+          </div>
 
-        <button class="btn btn-md btn-accent">APPLICA</button>
-    </form>
+          <button class="btn btn-md btn-accent">APPLICA</button>
+        </form>
+      </div>
+    </transition>
+  </div>
 </template>
 
+
 <script>
+import StarSystemComponent from './starSystemComponent.vue';
+
 export default {
-    name: "FilterComponent",
-    data() {
-        return {
-                onlySponsor: false,
-                mostReviewed: false,
-                rating_min: 1,
-                rating_max: 5,
-        };
+  name: 'FilterComponent',
+  components: {
+    StarSystemComponent,
+  },
+  data() {
+    return {
+      isCollapsed: false,
+      onlySponsor: false,
+      mostReviewed: false,
+      rating_min: 1,
+      rating_max: 5,
+    };
+  },
+  methods: {
+    applyFilters(event) {
+      event.preventDefault();
+      const form = this.$refs.formMessage;
+      const formData = new FormData(form);
+      let filterToApply = {
+        rating_min: parseInt(document.querySelector('input[name="rating-min"]:checked').value),
+        rating_max: 5,
+        only_sponsorized: formData.get('only_sponsorized'),
+        most_reviewed: formData.get('most_reviewed'),
+      };
+      console.log(filterToApply);
     },
-    methods: {
-        applyFilters(event) {
-            event.preventDefault();
-            const form = this.$refs.formMessage;
-            const formData = new FormData(form);
-            let filterToApply = {
-                rating_min: parseInt(document.querySelector('input[name="rating-min"]:checked').value),
-                rating_max: 5,
-                only_sponsorized: formData.get("only_sponsorized"),
-                most_reviewed: formData.get("most_reviewed"),
-            };
-            console.log(filterToApply);
-        },
-    },
-    mounted() {},
+    ratingEvent() {
+
+    }
+  },
+
+  mounted() {},
 };
 </script>
 
-<style></style>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.3s ease;
+  opacity: 1;
+}
+.slide-enter,
+.slide-leave-to {
+transform: translateY(-50px);
+  opacity: 0;
+}
+</style>
+
