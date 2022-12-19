8cbdc1af1bad90f92d6ea62fbf519be8832da592
svalvastic <assogames2@gmail.com>
2022-12-19 20:21:53 +0100

    ✨ feat(404): view and animation

---
+++ resources/js/views/notFoundView.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="animation">
+      <lottie-animation
+        class="lottie container mx-auto h-[80vh]"
+        ref="anim"
+        :animationData="require('../../assets/notFound2.json')"
+        :loop="true"
+      />
+  </div>
+</template>
+
+<script>
+import LottieAnimation from 'lottie-web-vue' // import lottie-web-vue
+export default {
+    components: {
+        LottieAnimation
+    },
+     mounted() {
+        this.$parent.paddingHandling(true,1000);
+    },
+    beforeDestroy() {
+        this.$parent.paddingHandling(false);
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
