cfe95f7c2b04cd35956ebba25b64f1835d586768
svalvastic <assogames2@gmail.com>
2022-12-19 11:48:17 +0100

    ✨ feat(search): add filter component

--- resources/js/views/searchView.vue
+++ resources/js/views/searchView.vue
@@ -1,7 +1,23 @@
 <template>
-    <div class="container mx-auto py-10">
-        <div class="flex flex-col sm:flex-row flex-wrap gap-8 justify-center">
-            <router-link :to="'/profile/'+ user.slug" v-for="(user, index) in users.data" :key="index">
+    <div class="container mx-auto py-20">
+        <h1 class="text-4xl font-bold py-5">
+            Risultati ricerca:
+            {{ this.$route.query.search.split("-").join(" ") }}
+        </h1>
+        <div class="mt-12">
+            <div class="totals flex flex-col justify-between">
+                <filter-component />
+                <p>Professionisti totali: {{ users.total }}</p>
+            </div>
+        </div>
+        <div
+            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
+        >
+            <router-link
+                :to="'/profile/' + user.slug"
+                v-for="(user, index) in users.data"
+                :key="index"
+            >
                 <user-card-component :user="user" />
             </router-link>
         </div>
@@ -12,9 +28,10 @@
 </template>
 
 <script>
+import FilterComponent from "../components/filterComponent.vue";
 import UserCardComponent from "../components/UserCardComponent.vue";
 export default {
-    components: { UserCardComponent },
+    components: { UserCardComponent, FilterComponent },
 
     data: () => ({
         users: [],
