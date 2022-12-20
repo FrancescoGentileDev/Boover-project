d20dfe06e34c8286321817611faa5599a652a06f
Hugocod <u.medioli@icloud.com>
2022-12-20 04:03:24 +0100

    styling usersSection

--- resources/js/sections/UsersShowcaseSection.vue
+++ resources/js/sections/UsersShowcaseSection.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wrapper pt-24 pb-24 z-10 bg-primary">
-        <div class="w-11/12 m-auto p-6">
-            <h1 class="text-5xl primary text-center text-white">
+        <div class="w-11/12 m-auto p-6 mb-4">
+            <h1 class="text-5xl text-center text-white mb-5">
                 Get inspired with our top freelancers
             </h1>
         </div>
@@ -37,39 +37,56 @@
                 [640, 2],
                 [768, 3],
                 [1024, 4],
-                [1280, 5],
+                [1280, 4],
             ]"
         >
-            <!--   <div class="avatar online">
-                <div class="w-24 rounded-full">
-                    <img src="https://placeimg.com/192/192/people" />
-                </div>
-            </div> -->
-
             <!-- slide-->
             <slide
                 v-for="(user, index) in users.slice(0, 20)"
                 :key="user + index"
                 class="relative p-6"
             >
-                <div class="flex flex-col bg-base-200 p-10 rounded-xl">
-                    <div class="avatar online m-auto">
-                        <div class="w-24 mask mask-squircle">
+                <div class="flex flex-col rounded-xl">
+                    <div class="avatar m-auto">
+                        <div class="w-40 rounded-full">
                             <img :src="user.avatar" />
                         </div>
                     </div>
-                    <h1 class="text-center py-3 font-semibold text-lg">
+
+                    <h1
+                        class="text-center py-3 font-semibold text-lg text-white"
+                    >
                         {{ user.name }}
                     </h1>
-                    <h1 class="text-center py-6"></h1>
 
-                    <div class="flex flex-wrap gap-2 justify-center">
-                        <div class="badge badge-primary">musicista</div>
-                        <div class="badge badge-primary">web designer</div>
-                        <div class="badge badge-primary">neutral designer</div>
-                        <div class="badge badge-primary">web designer</div>
-                        <div class="badge badge-primary">neutral designer</div>
-                    </div>
+                    <!--   <h1
+                        class="text-center py-3 font-semibold text-sm text-white"
+                    >
+                        utente silver
+                        <i class="fa-solid fa-medal"></i>
+                    </h1> -->
+
+                    <!--             <div class="flex flex-wrap gap-2 justify-center">
+                        <div class="badge badge-accent badge-outline">
+                            musicista
+                        </div>
+                        <div class="badge badge-accent badge-outline">
+                            web designer
+                        </div>
+                        <div class="badge badge-accent badge-outline">
+                            neutral designer
+                        </div>
+                        <div class="badge badge-accent badge-outline">
+                            web designer
+                        </div>
+                        <div class="badge badge-accent badge-outline">
+                            neutral designer
+                        </div>
+                    </div> -->
+
+                    <button @click="viewProfile(user.user_id)">
+                        See Profile
+                    </button>
                 </div>
             </slide>
         </carousel>
@@ -117,6 +134,7 @@ export default {
                     }
                 });
         },
+        //
     },
     mounted() {
         this.getUsersData();
