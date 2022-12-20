70e0c8bf9fef31b566e3993d7e0d6152e1fa3905
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-20 18:05:39 +0100

    better theme button style

--- resources/views/dashboard/settings/index.blade.php
+++ resources/views/dashboard/settings/index.blade.php
@@ -6,11 +6,15 @@
             class="basis-2/4 text-center px-6 py-2 font-medium tracking-wide base-100 capitalize transition-colors duration-300 transform bg-base-300 rounded-lg">
             Impostazioni
         </div>
-        <div class="form-control">
+        <div class="form-control mt-5">
             <form id="theme-form" method="POST" action="{{ route('dashboard.themeSwitch') }}">
                 @csrf
                 <label class="label cursor-pointer w-1/4">
-                    <span class="label-text base-100 font-semibold">Toggle Theme Mode</span>
+                    <span class="label-text base-100 font-semibold">Attiva Tema @if (Auth::user()->theme_preference == 'dark')
+                            Chiaro
+                        @elseif(Auth::user()->theme_preference == 'light')
+                            Scuro
+                        @endif </span>
                     <input @if (Auth::user()->theme_preference == 'dark') checked @endif
                         onclick="document.getElementById('theme-form').submit()" id="toggleThemeInput" type="checkbox"
                         class="toggle" name="theme" />
