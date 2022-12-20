551992b487c5633a715c11911f950e8cd6c1309f
Carmyo <carmelodicristina@gmail.com>
2022-12-20 01:27:33 +0100

    change color span in hero

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -18,7 +18,7 @@
     <div class="w-full bg-center bg-cover h-[38rem]" style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
         <div class="flex items-center justify-center w-full h-full bg-gray-900/40">
             <div class="text-center">
-                <h1 class="text-3xl font-semibold text-white lg:text-4xl"> I <span class="text-blue-400">Migliori</span>  a portata di click!</h1>
+                <h1 class="text-3xl font-semibold text-white lg:text-4xl"> I <span class="text-secondary">Migliori</span>  a portata di click!</h1>
                 <button class="w-full px-4 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
             </div>
         </div>
