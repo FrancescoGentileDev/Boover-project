e011f9153bd58804189c4a1f5f57df99a2bfceb1
Carmyo <carmelodicristina@gmail.com>
2022-12-21 17:43:08 +0100

    change text in button register

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -138,7 +138,7 @@
             <input id="email" type="text" class="px-4 py-2 text-black bg-white border rounded-md sm:mx-2 dark:text-black dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" placeholder="Email Address">
 
             <button class="btn btn-primary">
-                Get Started
+                Inizia Subito!
             </button>
         </div>
     </div>
