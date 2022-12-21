e954e0562241ac6177a8df39dd8ee4af0e81d3c0
Carmyo <carmelodicristina@gmail.com>
2022-12-21 16:48:08 +0100

    cleaning hero code

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -1,11 +1,7 @@
 <template>
   <main>
-    <section>                         
-     
-                                       <!--TEMPLATE-->
-
-    <div class="w-full bg-center bg-cover h-[38rem] relative " style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
-        
+    <section>      
+      <div class="w-full bg-center bg-cover h-[38rem] relative " style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
         <div class="flex items-center justify-center w-full h-full bg-gray-900/40">
             <div class="text-center">
                 <h1 class="text-3xl font-semibold text-white lg:text-4xl"> I <span class="text-secondary">Migliori</span>  a portata di click!</h1>
@@ -18,8 +14,7 @@
             <p class="px-10 text-white text-8xl pb-5">{{ information.numericInfo }}</p>
            </div>           
         </div>
-    </div>
-</div>
+      </div>
     </section>    
    
     
