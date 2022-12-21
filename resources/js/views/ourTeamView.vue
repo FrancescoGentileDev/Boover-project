aaa25a67dfacd39f70166cfdbeff74d3078d9845
Carmyo <carmelodicristina@gmail.com>
2022-12-21 17:16:10 +0100

    contestualized text

--- resources/js/views/ourTeamView.vue
+++ resources/js/views/ourTeamView.vue
@@ -87,12 +87,11 @@
 <section class="bg-base-200 dark:bg-gray-900 mt-40"> 
     <div class="container px-6 py-10 mx-auto">
         <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">
-            Scopri cosa dicono i nostri <span class="text-secondary">clienti</span> 
+            Scopri cosa dicono i tuoi <span class="text-secondary">clienti</span> 
         </h1>
 
         <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error
-            alias, adipisci rem similique, at omnis eligendi optio eos harum.
+            Resta sempre sul pezzo grazie alle recensioni lasciate dagli utenti che ti contattano. Monitora costantemente i loro feedback e migliora di conseguenza il tuo lavoro.
         </p>
        <div class="grid grid-cols-1 gap-8 mx-auto mt-8 lg:grid-cols-2 xl:mt-10 max-w-7xl">
            <div v-for="(reviews,index) in reviews" :key="index" class="h-full" >
@@ -113,7 +112,7 @@
         </div>
         <!--Q&A CONTAINER-->
      <div class="container mx-auto md:h-80">         
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 h-full ">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 h-full relative ">
                                    <!--Q&A BOX-->
              <div v-for="(faq,index) in faqs" :key="index">                
                   <div   tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
@@ -125,11 +124,9 @@
                     </div>
                  </div>
              </div>    
-             <button class="btn btn-primary justify-center">La tua domanda non è in elenco?</button>  
-             <button class="btn btn-primary justify-center">Contattaci</button>
-             
-        </div>
-        
+             <button class="btn btn-primary">La tua domanda non è in elenco?</button>  
+             <button class="btn btn-primary">Contattaci</button>             
+        </div>        
      </div>
     </section>
                                                                     <!--REGISTER-->
