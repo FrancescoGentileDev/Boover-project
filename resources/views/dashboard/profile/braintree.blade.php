f5f4ef05e771e5e76267a88373203bcdc3f747d8
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-21 10:36:00 +0100

    better sponsor buy view style

--- resources/views/dashboard/profile/braintree.blade.php
+++ resources/views/dashboard/profile/braintree.blade.php
@@ -1,92 +1,93 @@
 @extends('layouts.app')
 
 @section('head')
-{{-- Braintree DropIn Gateway JS --}}
-<script src="https://js.braintreegateway.com/web/dropin/1.24.0/js/dropin.min.js"></script>
+    {{-- Braintree DropIn Gateway JS --}}
+    <script src="https://js.braintreegateway.com/web/dropin/1.24.0/js/dropin.min.js"></script>
 @endsection
 
 @section('content')
-
-<section class="
+    <section class="
     flex
     flex-col
     justify-center
     items-center
     min-h-screen">
 
-    {{-- Info --}}
-    <article class="max-w-prose">
-        <h2>Sponsorship:</h2>
-        <p>By becoming one of our {{ $sponsor->type }} members you will be pushed to our front pages more frequently giving hiring agents the possibility to checkout your profile more often and for more job opportunities to come your way.</p>
-    </article>
+        {{-- Info --}}
+        <article class="max-w-prose">
+            <h2 class="text-2xl font-semibold">Sponsorizzazione:</h2>
+            <p>Diventando un membro {{ $sponsor->type }} comparirai nelle riceche in Homepage più frequentemente
+                dando la possibilità ai recruiter di visualizzare il tuo profilo più spesso e quindi ottenere
+                maggiori proposte di lavoro.</p>
+        </article>
 
-    {{-- FORM --}}
-    <form method="POST" id="payment-form" action="{{ route('dashboard.sponsor.checkout') }}"
-        class="
+        {{-- FORM --}}
+        <form method="POST" id="payment-form" action="{{ route('dashboard.sponsor.checkout') }}"
+            class="
             container
             flex
             flex-col
             items-center
             p-4
             lg:p-8
-        "
-    >
-        @csrf
-        <section class="
+        ">
+            @csrf
+            <section class="
             max-w-prose
             min-w-full
             md:min-w-[50%]
         ">
-            <label for="amount" class="input-wrapper amount-wrapper">
-                <div class="flex justify-between">
-                    <h3 class="
+                <label for="amount" class="input-wrapper amount-wrapper">
+                    <div class="flex justify-between">
+                        <h3
+                            class="
                         text-xl
                         font-bold
                         text-violet-500
-                    ">{{ $sponsor->type }} Sponsorship</h3>
-                    <p class="text-2xl"><strong>$ {{ $sponsor->price }}</strong></p>
+                    ">
+                            {{ $sponsor->type }} Sponsorship</h3>
+                        <p class="text-2xl"><strong>$ {{ $sponsor->price }}</strong></p>
+                    </div>
+                    <input type="hidden" name="sponsor" value="{{ $sponsor->id }}" />
+                </label>
+
+                <div class="bt-drop-in-wrapper mt-3">
+                    <div id="bt-dropin"></div>
                 </div>
-                <input type="hidden" name="sponsor" value="{{ $sponsor->id }}" />
-            </label>
+            </section>
 
-            <div class="bt-drop-in-wrapper mt-3">
-                <div id="bt-dropin"></div>
-            </div>
-        </section>
+            <input id="nonce" name="payment_method_nonce" type="hidden" value="" />
+            <button class="button btn btn-primary mt-4" type="submit">Effettua Sponsorizzazione</button>
+        </form>
+    </section>
 
-        <input id="nonce" name="payment_method_nonce" type="hidden" value="" />
-        <button class="button btn btn-primary mt-4" type="submit">Purchase Sponsorship</button>
-    </form>
-</section>
+    <script>
+        var form = document.querySelector('#payment-form');
+        var client_token = "{{ $clientToken }}";
 
-<script>
-    var form = document.querySelector('#payment-form');
-    var client_token = "{{ $clientToken }}";
-
-    braintree.dropin.create({
-      authorization: client_token,
-      selector: '#bt-dropin',
-      paypal: {
-        flow: 'vault'
-      },
-    }, function (createErr, instance) {
-      if (createErr) {
-        console.log('Create Error', createErr);
-        return;
-      }
-      form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        instance.requestPaymentMethod(function (err, payload) {
-          if (err) {
-            console.log('Request Payment Method Error', err);
-            return;
-          }
-          // Add the nonce to the form and submit
-          document.querySelector('#nonce').value = payload.nonce;
-          form.submit();
+        braintree.dropin.create({
+            authorization: client_token,
+            selector: '#bt-dropin',
+            paypal: {
+                flow: 'vault'
+            },
+        }, function(createErr, instance) {
+            if (createErr) {
+                console.log('Create Error', createErr);
+                return;
+            }
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                instance.requestPaymentMethod(function(err, payload) {
+                    if (err) {
+                        console.log('Request Payment Method Error', err);
+                        return;
+                    }
+                    // Add the nonce to the form and submit
+                    document.querySelector('#nonce').value = payload.nonce;
+                    form.submit();
+                });
+            });
         });
-      });
-    });
-</script>
-
+    </script>
 @endsection
