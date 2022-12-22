5af5dd9262f39e3d46f584750179a0250655edd2
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-22 23:04:43 +0100

    better backend inbox title

--- resources/views/dashboard/inboxes/index.blade.php
+++ resources/views/dashboard/inboxes/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="flex flex-row justify-center">
         <div
-            class="basis-2/4 text-center my-7 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg">
+            class="basis-full text-center my-7 px-6 py-2 font-medium tracking-wide capitalize transition-colors duration-300 transform bg-base-300 base-100 rounded-lg">
             I tuoi Messaggi
         </div>
     </div>
@@ -15,9 +15,9 @@
         {{ $inboxes->links() }}
     </div>
 
-    <ul id="inbox-list" class="w-full md:w-3/4 flex flex-col gap-3 mx-auto mt-4 mb-8">
+    <ul id="inbox-list" class="flex flex-col gap-3 mx-auto mt-4 mb-8">
         @foreach ($inboxes as $inbox)
-            <li class="px-5 py-2 bg-base-200 rounded-lg shadow-md
+            <li class="w-full px-5 py-2 bg-base-200 rounded-lg shadow-md
         hover:bg-base-300">
                 <a href="{{ route('dashboard.inboxes.show', $inbox->id) }}">
                     <p class="font-semibold text-xl">{{ $inbox->title }}</p>
