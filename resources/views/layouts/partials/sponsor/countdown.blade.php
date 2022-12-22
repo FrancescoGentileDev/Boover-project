a40130548a11b8f00e8d2a8547800bec6fe22c01
ValerioScharizza <valerio.schiarizza@live.it>
2022-12-22 22:58:22 +0100

    translated sponsor countdown

--- resources/views/layouts/partials/sponsor/countdown.blade.php
+++ resources/views/layouts/partials/sponsor/countdown.blade.php
@@ -1,34 +1,31 @@
-
 <h1 class="text-center" id="end">Il tuo boost scade tra...</h1>
 <div class="flex justify-center text-center">
 
     <div class="flex flex-col align-center justify-center">
-      <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
-        <span id="days"  style="--value:15;"></span>
-      </span>
-      days
+        <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
+            <span id="days" style="--value:15;"></span>
+        </span>
+        Giorni
     </div>
 
     <div class="flex flex-col">
-      <span class=" p-2  countdown font-mono text-4xl md:text-7xl ">
-        <span id="hours" style="--value:10;"></span>
-      </span>
-      hours
+        <span class=" p-2  countdown font-mono text-4xl md:text-7xl ">
+            <span id="hours" style="--value:10;"></span>
+        </span>
+        Ore
     </div>
 
     <div class="flex flex-col">
-      <span class=" p-2  countdown font-mono text-4xl md:text-7xl">
-        <span id="mins" style="--value:24;"></span>
-      </span>
-      min
+        <span class=" p-2  countdown font-mono text-4xl md:text-7xl">
+            <span id="mins" style="--value:24;"></span>
+        </span>
+        Minuti
     </div>
 
     <div class="flex flex-col">
-      <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
-        <span id="secs" style="--value:22;"></span>
-      </span>
-      sec
+        <span class=" p-2 countdown font-mono text-4xl md:text-7xl">
+            <span id="secs" style="--value:22;"></span>
+        </span>
+        Secondi
     </div>
 </div>
-
-
